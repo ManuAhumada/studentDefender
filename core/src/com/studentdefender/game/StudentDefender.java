1f1152458ad9d8ac9593005be74acaa6860851f6
Manuel Ahumada <manu06ahumada@gmail.com>
2019-07-24 18:57:30 -0300

    Primer commit

---
+++ core/src/com/studentdefender/game/StudentDefender.java
@@ -0,0 +1,25 @@
+package com.studentdefender.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class StudentDefender extends ApplicationAdapter {
+	SpriteBatch batch;
+
+	public void create() {
+		batch = new SpriteBatch();
+	}
+
+	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.end();
+	}
+
+	public void dispose() {
+		batch.dispose();
+	}
+}
